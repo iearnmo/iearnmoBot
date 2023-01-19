5789491bd3a5e0cf0115f05e1055a43e2b293ec3
denverku <magicph26@gmail.com>
2023-01-20 02:41:23 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const login = require("facebook-chat-api");
 
 // Create simple echo bot
-login({email: "09157400849", password: "nini09"}, (err, api) => {
+login({email: "rivers.zeronine@gmail.com", password: "nini09"}, (err, api) => {
     if(err) return console.error(err);
 
     api.listen((err, message) => {
