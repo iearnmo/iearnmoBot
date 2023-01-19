16095e0ad031d8beef86576e55c7f250164482fc
denverku <magicph26@gmail.com>
2023-01-20 02:23:25 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,22 +2,22 @@ const router = require('express').Router();
 const facebook = require('fb-messenger-bot-api');
 const messagingClient = new facebook.FacebookMessagingAPIClient('EAAZA1F3dtHAkBAEdf2T4dBC6tHN3GZBAT9QNmqjXiz8ZBXlWB4cnLHbnSVCHpfJgP0ZBBjSiBdI4q5QLggQKDZAABFXeGSsqtZAJhfHsH4Ei1CJwWXu6dOrm7GcPngEw4ifRRsoZBZCjWeXDvbgyMloa3RsopxZCT7qZBjxmbBb3oTCKrsQyLjgUVHeVJlefBCtzQZD');
 const messageParser = facebook.FacebookMessageParser;
-...
+
 router.get('/api/webhook',facebook.ValidateWebhook.validateServer);
 router.post('/api/webhook', (req, res) => {
     const incomingMessages = messageParser.parsePayload(req.body);  
-    ...
+    
     messagingClient.markSeen(senderId)
         .then(() => client.toggleTyping(senderId,true))
         .catch((err) => console.log(error));
-    ...
+    
     //promise based reaction on message send confirmation
     messagingClient.sendTextMessage(senderId, 'Hello')
         .then((result) => console.log(`Result sent with: ${result}`));
-    ...
+    
     //callback based reaction on message confirmation
     messagingClient.sendTextMessage(senderId, 'Hello',(result) => console.log(`Result sent with: ${result}`));
-    ...
+    
     //silent message sending
     messagingClient.sendTextMessage(senderId,'Hello');
 })
